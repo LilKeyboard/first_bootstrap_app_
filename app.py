9bfa30eebbfe1ba005d0f635be60ec9f75335ed3
LilKeyboard <wbalcerzak97@o2.pl>
2023-04-11 15:52:00 +0200

    flask bootstrap-theme.css lib remove

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 #testowy commit
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
